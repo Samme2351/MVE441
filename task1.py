cd86e6abeb6a83e32e2beaa68280f5932182ae77
tonygromer <gromer@chalmers.se>
2024-04-08 14:24:40 +0200

    print stuff

--- task1.py
+++ task1.py
@@ -1,5 +1,11 @@
 import numpy as np
 import pandas as pd
 
+df = pd.read_csv('Data/TCGAdata.txt', sep=" " ,header=0)
+labels_df = pd.read_csv('Data/TCGAlabels', sep=" " ,header=0)
 
+#Standardizing the rows (transposing as fit_transform standardizes along columns)
+
+print(df)
+print(labels_df)
 
