97412163818dec1d23e5ef67143b0d543ac158b4
tonygromer <gromer@chalmers.se>
2024-04-10 14:01:12 +0200

    ?

--- task1.py
+++ task1.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
+from sklearn.decomposition import PCA
+
 
 df = pd.read_csv('Data/TCGAdata.txt', sep=" " ,header=0)
 labels_df = pd.read_csv('Data/TCGAlabels', sep=" " ,header=0)
 
-#Standardizing the rows (transposing as fit_transform standardizes along columns)
 
 #print(df)
 #print(labels_df)
@@ -19,5 +20,7 @@ working_data = df.tail(n_rows-m_test)
 
 #print(test_data)
 #print(working_data)
+#Standardizing the rows (transposing as fit_transform standardizes along columns)?
+# Use PCA and cross-validation
 
 
