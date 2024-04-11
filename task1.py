bdec22edcaff2218441dc6d0571de981d3565752
tonygromer <gromer@chalmers.se>
2024-04-11 14:27:32 +0200

    untransform???

--- task1.py
+++ task1.py
@@ -2,7 +2,11 @@ import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC  
+from tqdm import tqdm
 
+scaler = preprocessing.StandardScaler()
 
 df = pd.read_csv('Data/TCGAdata.txt', sep=" " ,header=0)
 labels_df = pd.read_csv('Data/TCGAlabels', sep=" " ,header=0)
@@ -17,10 +21,12 @@ m_test = 900
 
 test_data = df.head(m_test)
 working_data = df.tail(n_rows-m_test)
+scaled_work_data = pd.DataFrame(scaler.fit_transform(working_data.transpose()), columns=df.columns)
 
 #print(test_data)
 #print(working_data)
 #Standardizing the rows (transposing as fit_transform standardizes along columns)?
 # Use PCA and cross-validation
+
 
 
