e6840f14c4732a93b644baa75e358caf3068a8bf
Miralleyan <odesjo@chalmers.se>
2024-04-16 14:52:29 +0200

    save

--- result.py
+++ result.py
@@ -5,8 +5,11 @@ data2 = pd.read_csv('run1/data.csv', sep=" " ,header=0)
 data3 = pd.read_csv('run2/data.csv', sep=" " ,header=0)
 data4 = pd.read_csv('run3/data.csv', sep=" " ,header=0)
 data5 = pd.read_csv('run4/data.csv', sep=" " ,header=0)
+data6 = pd.read_csv('data_1.csv', sep=" " ,header=0)
+data7 = pd.read_csv('data_2.csv', sep=" " ,header=0)
+
 print(data1)
 print(data2)
-data = (data1+data2+data3+data4+data5).div(5)
-
-data.to_csv('./mean.csv', sep=" ")
+data = (data1+data2+data3+data4+data5+data6+data7).div(7)
+data.style
+#data.to_csv('./mean.csv', sep=" ")
