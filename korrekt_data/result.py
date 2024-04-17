9b83141d04a29bec748c830d3b13ce336c3cd868
Miralleyan <odesjo@chalmers.se>
2024-04-17 16:14:28 +0200

    png:s

--- korrekt_data/result.py
+++ korrekt_data/result.py
@@ -2,16 +2,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
-labels = pd.read_csv('data_1.csv', sep=" " ,header=0)
-data1 = pd.read_csv('data_1.csv', sep=" " ,header=0).to_numpy()
-data2 = pd.read_csv('data_2.csv', sep=" " ,header=0).to_numpy()
-data3 = pd.read_csv('data_3.csv', sep=" " ,header=0).to_numpy()
-data4 = pd.read_csv('data_4.csv', sep=" " ,header=0).to_numpy()
-data5 = pd.read_csv('data_5.csv', sep=" " ,header=0).to_numpy()
-#data6 = pd.read_csv('data_6.csv', sep=" " ,header=0).to_numpy()
-#data7 = pd.read_csv('data_7.csv', sep=" " ,header=0).to_numpy()
+labels = pd.read_csv("data_1.csv", sep=" " ,header=0, index_col=0)
+data1 = pd.read_csv("data_1.csv", sep=" " ,header=0, index_col=0).to_numpy()
+data2 = pd.read_csv('data_2.csv', sep=" " ,header=0, index_col=0).to_numpy()
+data3 = pd.read_csv('data_3.csv', sep=" " ,header=0, index_col=0).to_numpy()
+data4 = pd.read_csv('data_4.csv', sep=" " ,header=0, index_col=0).to_numpy()
+data5 = pd.read_csv('data_5.csv', sep=" " ,header=0, index_col=0).to_numpy()
+data6 = pd.read_csv('data_6.csv', sep=" " ,header=0, index_col=0).to_numpy()
+data7 = pd.read_csv('data_7.csv', sep=" " ,header=0, index_col=0).to_numpy()
+data8 = pd.read_csv('data_8.csv', sep=" " ,header=0, index_col=0).to_numpy()
+data9 = pd.read_csv('data_9.csv', sep=" " ,header=0, index_col=0).to_numpy()
+data10 = pd.read_csv('data_10.csv', sep=" " ,header=0, index_col=0).to_numpy()
+data11 = pd.read_csv('data_11.csv', sep=" " ,header=0, index_col=0).to_numpy()
 
-data = (data1+data2+data3+data4+data5)/5#+data6+data7)/7
+
+
+
+data = (data1+data2+data3+data4+data5+data6+data7+data8+data9+data10+data11)/11
 
 x = [0.7, 0.8, 0.9]
 fig, axs = plt.subplots(2,3)
@@ -25,10 +32,9 @@ for j in range(3):
         s += 1
 
 for ax in axs.flat:
-    ax.set(xlabel='Precentage of data used for training', ylabel='Error')
-
-for ax in axs.flat:
-    ax.label_outer()
+    ax.set(xlabel='Precentage of data used for training', ylabel='Error',ylim =[0,0.012] )
+#for ax in axs.flat:
+#    ax.label_outer()
 
 
 location = 0 # For the best location
@@ -49,11 +55,10 @@ plt.legend()
 plt.show()
 '''
 
-#data.to_csv('./mean.csv', sep=" ")
 
 #Misses
 
-labels = pd.read_csv('data_miss.csv', sep=" " ,header=0, index_col=0)
+labels = pd.read_csv('data_miss_1.csv', sep=" " ,header=0, index_col=0)
 data1 = pd.read_csv('data_miss_1.csv', sep=" " ,header=0, index_col=0).to_numpy()
 data2 = pd.read_csv('data_miss_2.csv', sep=" " ,header=0, index_col=0).to_numpy()
 data3 = pd.read_csv('data_miss_3.csv', sep=" " ,header=0, index_col=0).to_numpy()
@@ -61,37 +66,48 @@ data4 = pd.read_csv('data_miss_4.csv', sep=" " ,header=0, index_col=0).to_numpy(
 data5 = pd.read_csv('data_miss_5.csv', sep=" " ,header=0, index_col=0).to_numpy()
 data6 = pd.read_csv('data_miss_6.csv', sep=" " ,header=0, index_col=0).to_numpy()
 data7 = pd.read_csv('data_miss_7.csv', sep=" " ,header=0, index_col=0).to_numpy()
+data8 = pd.read_csv('data_miss_8.csv', sep=" " ,header=0, index_col=0).to_numpy()
+data9 = pd.read_csv('data_miss_9.csv', sep=" " ,header=0, index_col=0).to_numpy()
+data10 = pd.read_csv('data_miss_10.csv', sep=" " ,header=0, index_col=0).to_numpy()
+data11 = pd.read_csv('data_miss_11.csv', sep=" " ,header=0, index_col=0).to_numpy()
 
-data = (data1+data2+data3+data4+data5+data6+data7)/7
+data = (data1+data2+data3+data4+data5+data6+data7+data8+data9+data10+data11)/11
 
 
-x = [0.7, 0.8, 0.9]
-fig, axs = plt.subplots(6,3)
+def plot(version):
+    x = [0.7, 0.8, 0.9]
+    fig, axs = plt.subplots(2,3)
 
-type = ['-', '*-', '--']
-color = ["blue", "green", "red", "purple", "pink", "black"]
-s= 0
-for k in range(3):
-    for j in range(2):
-        for i in range(3):
+    color = ["blue", "green", "red"]
+    s= 0
+    for k in range(3):
+        for j in range(2):
+            for i in range(3):
+                axs[j,k].plot(x, [data[i][s+18*version], data[i][6+18*version+s], data[i][12+18*version + s]], color = color[i])
+                axs[j,k].set_title(labels.columns[s+18*version][:-6])
+            s += 1
+            '''
             axs[j,k].plot(x, [data[i][s], data[i][6+s], data[i][12+s]], color = color[i])
-            axs[j,k].set_title(lab.columns[s][:-6])
+            axs[j,k].set_title(labels.columns[s][:-6])
 
             axs[2+j,k].plot(x, [data[i][18+s], data[i][24+s], data[i][30+s]], color = color[i])
-            axs[2+j,k].set_title(lab.columns[18+s][:-6])
+            axs[2+j,k].set_title(labels.columns[18+s][:-6])
 
             axs[4+j,k].plot(x, [data[i][36+s], data[i][42+s], data[i][48+s]], color = color[i])
-            axs[4+j,k].set_title(lab.columns[36+s][:-6])
-        s += 1
+            axs[4+j,k].set_title(labels.columns[36+s][:-6])
+            '''
+            
+    y = [0.25, 0.65,1]
 
-for ax in axs.flat:
-    ax.set(xlabel='Precentage of data used for training', ylabel='Error')
+    for ax in axs.flat:
+        ax.set(xlabel='Precentage of data used for training', ylabel='Error',ylim =[0,y[version]])
 
-for ax in axs.flat:
-    ax.label_outer()
+    location = 0 # For the best location
+    legend_drawn_flag = True
+    plt.legend(["Training error", "Cross-val error", "Test error"], loc=0, frameon=legend_drawn_flag)
+    plt.show()
 
 
-location = 0 # For the best location
-legend_drawn_flag = True
-plt.legend(["Training error", "Cross-val error", "Test error"], loc=0, frameon=legend_drawn_flag)
-plt.show()
+plot(0)
+plot(1)
+plot(2)
