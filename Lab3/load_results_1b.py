b1abb7a789e62fc1541a77f41ba96ba2267729bd
Samuel Martinsson <90909426+Samme2351@users.noreply.github.com>
2024-05-17 16:34:10 +0200

    add visualization of important features

--- Lab3/load_results_1b.py
+++ Lab3/load_results_1b.py
@@ -2,8 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from os.path import exists
+from matplotlib import colormaps
 
 df = pd.read_csv('../Data/CATSnDOGS.csv', sep="," ,header = 0)
+print(df)
 #print(list(df.columns))
 
 
@@ -23,7 +25,7 @@ while file_exists:
 
 # Sort LR scores
 LR_imp_features = [x for _, x in sorted(zip(lr_scores, list(df.columns)), key=lambda pair: pair[0], reverse=True)]
-#print(LR_imp_features[:100])
+print(LR_imp_features[:150])
 
 
 ## KB scores: f_classif scores
@@ -42,7 +44,7 @@ while file_exists:
 
 # Sort KB scores
 KB_imp_features = [x for _, x in sorted(zip(kb_scores, list(df.columns)), key=lambda pair: pair[0], reverse=True)]
-#print(KB_imp_features[:100])
+print(KB_imp_features[:150])
 
 
 ## NC scores: absolute distance between centroid and overall centroid
@@ -65,6 +67,15 @@ while file_exists:
 
 # Sort NC scores
 NC_imp_features = [x for _, x in sorted(zip(nc_scores, list(df.columns)), key=lambda pair: pair[0], reverse=True)]
-#print(NC_imp_features[:100])
+print(NC_imp_features[:200])
 
-print(set(LR_imp_features[:200]) & set(KB_imp_features[:200]) & set(NC_imp_features[:200]))
+imp_feat = list(set(LR_imp_features[:200]) & set(KB_imp_features[:200]) & set(NC_imp_features[:200]))
+imp_feat = [int(name.replace('V', '')) for name in imp_feat]
+print(imp_feat)
+imp_plt = df.iloc[20].to_numpy()
+imp_plt[imp_feat] = 255
+imp_plt = imp_plt.reshape((64,64)).transpose()
+#print(imp_plt[imp_feat])
+plt.imshow(imp_plt, cmap = colormaps['bone'])
+#plt.imshow(imp_plt.reshape((64, 64)))
+plt.show()
